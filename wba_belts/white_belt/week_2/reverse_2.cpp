f7e4a8299b94c60db0b795711530a8d2f4d5ea44
Kirill Vlasenkov <vlasenkov.kv@phystech.edu>
2020-09-23 23:53:01 +0300

    partially added week 2

---
+++ wba_belts/white_belt/week_2/reverse_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> Reversed(const vector<int> seq) {
+	vector<int> res;
+	for (int i = seq.size() -1; i >= 0; --i)
+		res.push_back(seq[i]);
+	return res;
+}
+
+/* int main()
+{
+	vector<int> nums = {1, 2 ,3 , 4, 5};
+	vector<int> lst = Reversed(nums);
+	for (auto x: lst)
+		cout << x << " ";
+	return 0;
+} */
