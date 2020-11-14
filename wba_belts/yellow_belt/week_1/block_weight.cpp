f1db1ed76840827888d4ba935c22a36627dc2e5f
Kirill <vlasenkov.kv@phystech.edu>
2020-11-15 02:45:29 +0300

    completed week 1

---
+++ wba_belts/yellow_belt/week_1/block_weight.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+
+/* int main() {
+    int16_t density;
+    uint32_t quantity;
+    std::cin >> quantity >> density;
+    std::vector<uint64_t> volume;
+
+    for (size_t i = 0; i < quantity; ++i) {
+        uint64_t a, b, c;
+        std::cin >> a >> b >> c;
+
+        volume.push_back(a * b * c);
+    }
+
+    uint64_t mass_sum = 0;
+    for (const auto &elem : volume) {
+        mass_sum += elem * density;
+    }
+
+    std::cout << mass_sum << std::endl;
+    return 0;
+}
+ */
