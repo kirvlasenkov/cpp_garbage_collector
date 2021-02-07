6c10dad0f780c608336ef107e9a3b6a2d4bf3909
Kirill <vlasenkov.kv@phystech.edu>
2021-02-07 09:26:13 +0300

    Delete wba_belts directory

--- wba_belts/white_belt/week_2/isPalindrom.cpp
+++
@@ -1,9 +0,0 @@
-bool IsPalindrom(string s) {
-   	// Определяет, является ли слово палиндромом или нет:)
-  	 for (int i = 0; i < s.size() / 2; ++i) {
-        if (s[i] != s[s.size() - i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
--- wba_belts/yellow_belt/week_3/sum_reverse_sort/sum_reverse_sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-
-int Sum(int x, int y);
-std::string Reverse(std::string s);
-void Sort(std::vector<int>& nums);
